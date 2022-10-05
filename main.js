7e6f13128e0965888cdd8bda65aa922a8e3f6af1
Samoge <samuelokwuchukwu4@gmail.com>
2022-10-05 10:37:47 +0100

    Updated

---
+++ main.js
@@ -0,0 +1,19 @@
+var hamburger = document.querySelector('.hamburger_menu')
+var showlinks = document.querySelector('.smoothmenu')
+// var backdrop = document.querySelector('.backdrop')
+
+function Addclass() {
+    hamburger.classList.toggle('show')
+    showlinks.classList.toggle('smoothmenushow')
+}
+
+ScrollReveal({
+    reset: true,
+    duration: 2000,
+    distance: "90px",
+  });
+
+  ScrollReveal().reveal(".headerright", {
+    delay: 100,
+    origin: "top-right",
+  });
